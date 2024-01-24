010b0c80aa39def52d8036d7754821d0656edc6d
kotovnik <kotovnik2024tbilisi@gmail.com>
2024-01-25 01:21:15 +0400

    fix: solve for generateOdds task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -361,8 +361,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return new Array(len).fill(0).map((item, index) => 2 * index + 1);
 }
 
 /**
